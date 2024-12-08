2d3ae28713393034170cc7d7d97eaeb43551fad1
eric-marin13 <ericmarn@gmail.com>
2024-12-08 09:42:04 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,9 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import './assets/global.css';
-const _ = require("lodash");
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
   .mount('#app')
-
-// Example vulnerable code (Prototype Pollution)
-const payload = JSON.parse('{"__proto__": {"polluted": "yes"}}');
-_.merge({}, payload);
